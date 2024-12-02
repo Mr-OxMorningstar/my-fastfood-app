f4531c5f512bb6ad248c0ed77f69bb6107c7887f
Morningstar <agreekidox@gmail.com>
2024-12-02 21:24:06 +0200

    npm mobile installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 
 import styles from "./page.module.css";
+var mobile = require('is-mobile');
 
 export default function Home() {
+  console.log(mobile())
+
   return (
     <div className={styles.page}>
       <p>hello world</p>
